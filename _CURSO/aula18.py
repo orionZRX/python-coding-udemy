5267610b5f6ad8d88db7a9b8e170cd5872686679
Marlon Bartz <orionzrx@outlook.com>
2023-04-21 18:26:30 -0300

    aula adicionada

---
+++ _CURSO/aula18.py
@@ -0,0 +1,22 @@
+# if / elif      / else
+# se / se não se / se não
+condicao1 = False
+condicao2 = False
+condicao3 = True
+condicao4 = True
+
+if condicao1:
+    print('Este é o código da condição 1')
+elif condicao2:
+    print('Este é o código da condição 2')
+elif condicao3:
+    print('Este é o código da condição 3')
+elif condicao4:
+    print('Este é o código da condição 4')
+else:
+    print('Nenhuma condição foi satisfeita')
+
+if 10 == 10:
+    print('Outro if')
+
+print('Fora do if')
