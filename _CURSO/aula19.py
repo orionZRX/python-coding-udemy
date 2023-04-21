3c89dfa15537bf8a22b86aa14f66b25d1b24acee
Marlon Bartz <orionzrx@outlook.com>
2023-04-21 18:45:42 -0300

    aula adicionada

---
+++ _CURSO/aula19.py
@@ -0,0 +1,19 @@
+"""
+Operadores de comparação (relacionais)
+OP      Significado         Exemplo (True)
+>       maior               2 > 1
+>=      maior ou igual      2 >= 2
+<       menor               1 < 2
+<=      menor ou igual      2 <= 2
+==      igual               'a' == 'a' / 1 == 1
+!=      diferente           'a' != 'b' / 1 != 2
+"""
+maior = 2 > 1
+maior_ou_igual = 2 >= 2
+menor = 1 < 2
+menor_ou_igual = 2 <= 2
+igual = 'a' == 'a'
+diferente = 'a' != 'b'
+print('VARIAVEIS DO MENU INTERATIVO: maior, maior_ou_igual,'
+      'menor, menor_ou_igual,'
+      'igual, diferente')
