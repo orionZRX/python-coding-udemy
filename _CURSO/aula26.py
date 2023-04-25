93acda47b2da072b1c6d48be43cfd99030777c04
Marlon Bartz <orionzrx@outlook.com>
2023-04-24 21:30:33 -0300

    aula adicionada

---
+++ _CURSO/aula26.py
@@ -0,0 +1,15 @@
+"""
+Formatação básica de strings
+s - string
+d - int
+f - float
+.<número de dígitos>f
+x ou X - Hexadecimal
+(Caractere)(><^)(quantidade)
+> - Esquerda
+< - Direita
+^ - Centro
+Sinal - + ou -
+Ex.: 0>-100,.1f
+Conversion flags - !r !s !a
+"""
