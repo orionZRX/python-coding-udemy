f040d8739be2e91a5b9f066814285148c93f0d11
Marlon Bartz <orionzrx@outlook.com>
2023-04-25 21:51:45 -0300

    aula adicionada

--- _CURSO/aula26.py
+++ _CURSO/aula26.py
@@ -13,3 +13,11 @@ Sinal - + ou -
 Ex.: 0>-100,.1f
 Conversion flags - !r !s !a
 """
+variavel = 'ABC'
+print(f'{variavel}.')
+print(f'{variavel: >10}.')
+print(f'{variavel: <10}.')
+print(f'{variavel:$^10}.')
+print(f'{1000.4213882130128382013012:,.1f}')
+print(f'O hexadecimal de 1500 é {1500:08X}')
+print(f'{variavel!r}')
